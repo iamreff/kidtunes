f97b8ea7bdb62bacbd2bf5bafb6366fbcdee440d
John Refford <jrefford@gmail.com>
2012-08-09 07:57:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kidtunes::Application.config.secret_token = '4160bbc8d05c985fef87df99d87978cc48b42916b3a490c7c77df5b167696feb0da8cdc61baecf82dc0f42658b6675caeea37ef0f6474e6ee31aa2b4ca3dfe1d'
