fd060810e9b4ec638b7f48b91023041b5c6e0c8a
John Refford <jrefford@gmail.com>
2012-08-14 08:28:24 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Home')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "kidtunes | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "kidtunes | Help")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "kidtunes | Contact")
+    end
+  end
+
+
+end
+
