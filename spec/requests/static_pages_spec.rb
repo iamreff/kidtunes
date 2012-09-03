495d6c43e1cc5872742ae78e615ba68e633f740e
John Refford <jrefford@gmail.com>
2012-09-03 15:38:19 -0400

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,60 +2,28 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+subject { page }
+
   describe "Home page" do
+  before { visit root_path }
 
-    it "should have the h1 'kidtunes'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'kidtunes')
-    end
-
-    it "should have the base title " do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "kidtunes")
-    end
-  end
-
-  it "should not have a custom page title" do
-    visit '/static_pages/home'
-    page.should have_selector('title', :text => "kidtunes")
+    it { should have_selector('h1', :text => 'kidtunes') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', :text => "Home") }
   end
 
   describe "Help page" do
+  before { visit help_path }
 
-    it "should have the h1 'kidtunes'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'kidtunes')
-    end
-
-    it "should have the base title " do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "kidtunes")
-    end
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
   
-  it "should not have a custom page title" do
-    visit '/static_pages/help'
-    page.should have_selector('title', :text => "kidtunes")
-  end
-
-
   describe "Contact page" do
+  before { visit contact_path }
 
-    it "should have the h1 'kidtunes'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'kidtunes')
-    end
-
-    it "should have the base title " do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => "kidtunes")
-    end
-
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
-  it "should not have a custom page title" do
-    visit '/static_pages/contact'
-    page.should have_selector('title', :text => "kidtunes")
-  end
-
 end
 
