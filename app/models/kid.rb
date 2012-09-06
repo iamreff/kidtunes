879c944cec11aa75d5b0fe1a6eef4be765dd4340
John Refford <jrefford@gmail.com>
2012-09-06 07:24:35 -0400

    STI User Model - Initial

---
+++ app/models/kid.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  fname      :string(255)
+#  lname      :string(255)
+#  email      :string(255)
+#  type       :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class Kid < User
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,11 @@ Kidtunes::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Added to fix "lazy loading" and support single table inheritance 
+  config.to_prepare {
+  %w[asset unix_asset host ads_host].each do |c|
+  require_dependency File.join("app","models","#{c}.rb")
+  end
+  }
 end
