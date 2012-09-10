ed97b63f6704d42ee4f99ad8449122e4a93809cf
John Refford <jrefford@gmail.com>
2012-09-10 07:33:45 -0400

    use thin via Profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120907143425) do
+ActiveRecord::Schema.define(:version => 20120907143426) do
 
   create_table "kids", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "parents", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
